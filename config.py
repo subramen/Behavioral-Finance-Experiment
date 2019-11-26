cbf3c23fd3695d5e85139995452f63dc5f7b81fc
suraj <suraj813@gmail.com>
2019-11-26 18:51:26 -0500

    reverting

--- config.py
+++ config.py
@@ -127,14 +127,14 @@ screen2 = html.Div(id='screen2',style={'display':'none'}, children = [
                             html.Tr([html.Th(style={'display':'block'}, children=['Available Cash']), \
                                      html.Th(style={'display':'block'}, children=['Stocks Held']), \
                                      html.Th(style={'display':'block'}, children=['Net Worth']), \
-                                     html.Th(style={'display':'none'}, children=['Today is'])
+                                     html.Th(style={'display':'block'}, children=['Today is'])
                                      ],
                                  style={'float':'left', 'display':'block'}),
                                      
                             html.Tr([html.Td(style={'display':'block', 'color':'green'}, id='cash-str', children="${}".format(status0['cash'])), \
                                      html.Td(style={'display':'block'}, id='stock-str', children="0"), \
                                      html.Td(style={'display':'block'}, id='position-str', children="$0"), \
-                                     html.Td(style={'display':'none'}, id='today-str', children="09/16 09:30")],
+                                     html.Td(style={'display':'block'}, id='today-str', children="09/16 09:30")],
                                  style={'float':'left', 'display':'block'}),
                         ])]),
                                      
